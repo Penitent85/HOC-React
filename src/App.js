9372217561712b3a9917e374f2bf8e5eafa13be2
Mahdi <penitent85@gmail.com>
2024-08-01 20:18:49 +0300

    Add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoList from "./TodoList";
+import SearchUsers from "./UserList";
+import SearchTodos from "./TodoList";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>HOC</h1>
+      <div className="section">
+        <div>
+          {" "}
+          <SearchUsers />
+        </div>
+        <div>
+          <SearchTodos />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/UserList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import HOC from "./HOC";
+
+const UserList = ({ data }) => {
+  let renderUsers = data.map((user) => {
+    return (
+      <div key={user.id}>
+        <p>
+          <strong> {user.name} </strong>
+        </p>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <div>{renderUsers}</div>
+    </div>
+  );
+};
+const SearchUsers = HOC(UserList, "users");
+export default SearchUsers;
